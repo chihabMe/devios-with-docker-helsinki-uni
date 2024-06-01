e0724cca75e883284027f6d034e1ab694df99c71
chihabMe <chihab.mg.me@gmail.com>
2024-06-01 15:41:24 +0100

    part 1 first chapter

---
+++ part1/exercise_1.1/solution.sh
@@ -0,0 +1,7 @@
+docker   run -d 1.25-alpine 
+docker   run -d django 
+docker run -d mysql
+
+
+docker  stop 57faf16d7363 
+docker  stop 4b01e0ac2632
---
+++ part1/exercise_1.2/solution.sh
@@ -0,0 +1,4 @@
+
+## i need the images so i cant remove all of them but i included anexapmle of how to remove all images
+
+docker image prune 
